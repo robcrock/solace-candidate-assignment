2e108a340b2686a3a2b5d8df9102c68b983d0edc
Robert Crocker <rcrocker13@gmail.com>
2025-02-28 21:13:15 -0600

    Move all cell content to the top of the cells.

--- src/app/data-table.tsx
+++ src/app/data-table.tsx
@@ -66,7 +66,7 @@ export const DataTable = <TData, TValue>({
                 data-state={row.getIsSelected() && "selected"}
               >
                 {row.getVisibleCells().map((cell) => (
-                  <TableCell key={cell.id}>
+                  <TableCell key={cell.id} className="align-top">
                     {flexRender(cell.column.columnDef.cell, cell.getContext())}
                   </TableCell>
                 ))}
